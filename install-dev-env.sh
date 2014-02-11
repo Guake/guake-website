1904c2f0f382a718fe14d0763ce4aa5459f144e0
Gaetan SEMET <gaetan@xeberon.net>
2014-02-11 23:32:14 +0100

    initializing repo
    following http://blog.elenakolevska.com/using-grunt-with-laravel-and-bootstrap/
    
    Signed-off-by: Gaetan SEMET <gaetan@xeberon.net>

---
+++ install-dev-env.sh
@@ -0,0 +1,6 @@
+npm install grunt --save-dev
+npm install grunt-contrib-concat --save-dev
+npm install grunt-contrib-less --save-dev
+npm install grunt-contrib-uglify --save-dev
+npm install grunt-contrib-watch --save-dev
+npm install grunt-phpunit --save-dev
