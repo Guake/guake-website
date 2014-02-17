d79680c8fbf381de9e105a4b4d89b161161b15c2
Gaetan Semet <gaetan@xeberon.net>
2014-02-17 01:50:58 +0100

    working on background. fixed and scroll ok, still work to do on parallax
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- src/js/src/guake/guake.js
+++ src/js/src/guake/guake.js
@@ -13,7 +13,32 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 700, 'easeInOutExpo');
         event.preventDefault();
     });
+});
+
+
+$(document).ready(function(){
+    //$('#nav').localScroll(800);
+
+    RepositionNav();
+
+    $(window).resize(function(){
+        RepositionNav();
+    });
+
+    //.parallax(xPosition, adjuster, inertia, outerHeight) options:
+    //xPosition - Horizontal position of the element
+    //adjuster - y position to start from
+    //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+    //outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+    // $('.intro-body').parallax(0, 0, 0.1, false);
+
+    var deck = new $.scrolldeck({
+        slides: '.slide',
+        buttons: '#nav li a',
+        easing: 'easeInOutExpo'
+    });
+
 });
